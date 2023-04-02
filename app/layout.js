4fe1788b7ed0f6bbcd0c1a3a92b47d3acdbb8211
snosw <54971768+snosw@users.noreply.github.com>
2023-04-02 21:18:35 +0700

    Added menu button and route to home

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,10 @@ import Image from "next/image";
 import { useState } from "react";
 
 import logo from "@/public/logo.png";
+import menuIcon from "@/public/hamburger.svg";
+import xIcon from "@/public/x.svg";
 import CompanyList from "./components/CompanyList";
+import Link from "next/link";
 
 export default function RootLayout({ children }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -24,8 +27,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <div className={styles.topbar}>
-          <div className={styles.topbarLogo} onClick={openNav}>
-            <Image src={logo} width="185" height="70" alt={"Logo"} />
+          <div className={styles.menuBar}>
+            <Link href="/">
+              <Image src={logo} width={185} height={70} alt={"Logo"} />
+            </Link>
+            <div className={styles.menuIcon} onClick={openNav}>
+              <Image src={menuIcon} width={40} height={40} />
+            </div>
           </div>
         </div>
         <div
@@ -33,16 +41,15 @@ export default function RootLayout({ children }) {
             styles.sidebar + " " + (sidebarOpen ? styles.sidebarOpen : "")
           }
         >
-          <div className={styles.sidebarLogo}>
-            <Image
-              src={logo}
-              width="185"
-              height="70"
-              alt={"Logo"}
-              onClick={closeNav}
-            />
+          <div className={styles.menuBar}>
+            <Link href="/">
+              <Image src={logo} width={185} height={70} alt={"Logo"} />
+            </Link>
+            <div className={styles.menuIcon} onClick={closeNav}>
+              <Image src={xIcon} width={40} height={40} />
+            </div>
           </div>
-          <CompanyList styleContext={"sidebarList"} onClick={closeNav} />
+          <CompanyList styleContext={"sidebarList"} />
         </div>
         <div className={"main"}>{children}</div>
       </body>
