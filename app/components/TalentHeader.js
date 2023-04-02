30a1b7641d01a94262dbf11e9d16c15617d21dc7
snosw <54971768+snosw@users.noreply.github.com>
2023-04-02 20:22:01 +0700

    Finished talent header

--- app/components/TalentHeader.js
+++ app/components/TalentHeader.js
@@ -3,29 +3,62 @@ import styles from "./Components.module.css";
 import useSWR from "swr";
 
 import Loading from "./loading";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function TalentHeader({ vtuberId }) {
-  // const fetchTalent = async () => {
-  //   console.log(
-  //     `${process.env.NEXT_PUBLIC_USER_API}/user/list?vtuber_id=${vtuberId}`
-  //   );
-  //   const res = await fetch(
-  //     `${process.env.NEXT_PUBLIC_USER_API}/user/list?vtuber_id=${vtuberId}`
-  //   );
-  //   const data = await res.json();
-  //   // console.log(data);
-  //   return data;
-  // };
-  // const { data: talent, error: talentError } = useSWR("talent", fetchTalent, {
-  //   onSuccess: (data) => {
-  //     console.log(data);
-  //   },
-  // });
+  const fetchTalent = async () => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_USER_API}/user/list?vtuber_id=${vtuberId}`
+    );
+    const data = await res.json();
+    return data.data[0];
+  };
 
+  const { data: talent, error: talentError } = useSWR("talent", fetchTalent);
+
+  console.log(talent);
+  // console.log(talent.details.banner);
   return (
     <>
-      {/* <div>{talent ? talent : <Loading />}</div> */}
-      <div>Test</div>
+      <div
+        className={styles.headerBanner}
+        style={{
+          backgroundImage: talent
+            ? `url("${talent.details.banner}=w1920-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj")`
+            : null,
+        }}
+      />
+      <div className={styles.headerContainer}>
+        {talent ? (
+          <>
+            <Link
+              href={`https://youtube.com/channel/${talent.details.id}`}
+              target="_blank"
+              className={styles.button}
+            >
+              <Image
+                src={talent.details.photo}
+                width={180}
+                height={180}
+                alt={talent.vtuber_name}
+              />
+            </Link>
+            <div className={styles.headerTextContainer}>
+              <h1>{talent.channel[0].channel_name}</h1>
+              <h4>
+                {parseInt(talent.details.subscriber_count).toLocaleString("en")}{" "}
+                subscribers
+              </h4>
+              <h5 className={styles.companyPill}>
+                {talent.companies[0].company.company_name}
+              </h5>
+            </div>
+          </>
+        ) : (
+          <Loading />
+        )}
+      </div>
     </>
   );
 }
